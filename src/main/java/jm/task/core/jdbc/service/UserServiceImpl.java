2776eb49c25090dfcf5181536d3f24ccb87a4dcf
Bulgakov Egor <egor_mrag1999@,ail.ru>
2023-02-17 20:15:13 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class UserServiceImpl extends Util implements UserService {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        userDaoJDBC.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        userDaoJDBC.saveUser(name, lastName, age);
+        System.out.println("User с именем – " + name + " добавлен в базу данных");
+    }
+
+    public void removeUserById(long id) {
+        userDaoJDBC.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = userDaoJDBC.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        userDaoJDBC.cleanUsersTable();
+    }
+}
